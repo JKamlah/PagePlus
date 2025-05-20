9f81ca5f5e19d4598ab20798bf2e59c393510618
JKamlah <jan.kamlah@gmail.com>
2025-05-20 13:14:36 +0200

    Update main.

--- pageplus/main.py
+++ pageplus/main.py
@@ -1,9 +1,7 @@
-from logging import DEBUG
-
-import typer
 import logging
 import warnings
 
+import typer
 
 # Silence!
 logging.getLogger("requests").setLevel(logging.WARNING)
@@ -22,22 +20,25 @@ warnings.filterwarnings("ignore", category=UserWarning, module="pydantic")
 
 
 
-from pageplus.cli import (system, analytics, validation, modification, export, workspace, projects,
-                          dinglehopper, llm, mets, ocr_kraken, ocr_tesseract, escriptorium, transkribus)
+from pageplus.cli import (litellm, system, analytics, validation, visualize, modification, ingest, export, workspace, projects,
+                          dinglehopper, gemini, mets, ocr_kraken, ocr_tesseract, escriptorium, transkribus)
 
 app = typer.Typer()
 app.add_typer(system.app, name="system", rich_help_panel="System")
 app.add_typer(escriptorium.app, name="escriptorium", rich_help_panel="Transcription-Platform")
 app.add_typer(transkribus.app, name="transkribus", rich_help_panel="Transcription-Platform")
 app.add_typer(dinglehopper.app, name="dinglehopper", rich_help_panel="PagePlus - external tools")
-app.add_typer(llm.app, name="llm", rich_help_panel="PagePlus - external tools")
+app.add_typer(litellm.app, name="litellm", rich_help_panel="PagePlus - external tools")
+app.add_typer(gemini.app, name="gemini", rich_help_panel="PagePlus - external tools")
 app.add_typer(ocr_kraken.app, name="kraken", rich_help_panel="Kraken - external tools")
 app.add_typer(ocr_tesseract.app, name="tesseract", rich_help_panel="Tesseract - external tools")
 app.add_typer(analytics.app, name="analytics", rich_help_panel="PagePlus")
 app.add_typer(validation.app, name="validation", rich_help_panel="PagePlus")
+#app.add_typer(visualize.app, name="visualize", rich_help_panel="PagePlus")
 app.add_typer(modification.app, name="modification", rich_help_panel="PagePlus")
 app.add_typer(mets.app, name="mets", rich_help_panel="PagePlus")
 app.add_typer(workspace.app, name="workspace", rich_help_panel="PagePlus")
+app.add_typer(ingest.app, name="ingest", rich_help_panel="PagePlus")
 app.add_typer(export.app, name="export", rich_help_panel="PagePlus")
 app.add_typer(projects.app, name="projects", rich_help_panel="PagePlus - projects")
 
